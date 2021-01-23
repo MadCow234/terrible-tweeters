6b8e66346beae375f69be91c0dc925b0af9caf4e
MadCow234 <MadCow234@gmail.com>
2021-01-23 04:09:07 -0600

    Optimize component caching

--- Terrible Tweeters/Assets/Scripts/Bird.cs
+++ Terrible Tweeters/Assets/Scripts/Bird.cs
@@ -6,41 +6,50 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     private Vector2 _startPosition;
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
         // Cache the bird's initial position
-        _startPosition = GetComponent<Rigidbody2D>().position;
+        _startPosition = _rigidbody2D.position;
         
         // Take the bird out of physics control
         // This is so we can aim it and it does not automatically fall to the ground
-        GetComponent<Rigidbody2D>().isKinematic = true;
+        _rigidbody2D.isKinematic = true;
     }
 
     private void OnMouseDown()
     {
-        GetComponent<SpriteRenderer>().color = Color.red;
+        _spriteRenderer.color = Color.red;
     }
 
     private void OnMouseUp()
     {
-        Vector2 currentPosition = GetComponent<Rigidbody2D>().position;
-        Vector2 direction = _startPosition - currentPosition;
+        var currentPosition = _rigidbody2D.position;
+        var direction = _startPosition - currentPosition;
         direction.Normalize();
         
         // Add the bird back to physics control so it can be launched
-        GetComponent<Rigidbody2D>().isKinematic = false;
-        GetComponent<Rigidbody2D>().AddForce(direction * 500);
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * 500);
         
-        GetComponent<SpriteRenderer>().color = Color.white;
+        _spriteRenderer.color = Color.white;
     }
 
     private void OnMouseDrag()
     {
         // Sync the bird position to the mouse position
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var transform1 = transform;
+        transform1.position = new Vector3(mousePosition.x, mousePosition.y, transform1.position.z);
     }
 
     // Update is called once per frame
