577c29441ec7b443a774be281485c3dd10275904
MadCow234 <MadCow234@gmail.com>
2021-01-23 04:04:13 -0600

    Enable pull back and launch of Bird

--- Terrible Tweeters/Assets/Scripts/Bird.cs
+++ Terrible Tweeters/Assets/Scripts/Bird.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    private Vector2 _startPosition;
+
     // Start is called before the first frame update
     private void Start()
     {
-        // Takes the bird out of physics control
+        // Cache the bird's initial position
+        _startPosition = GetComponent<Rigidbody2D>().position;
+        
+        // Take the bird out of physics control
         // This is so we can aim it and it does not automatically fall to the ground
         GetComponent<Rigidbody2D>().isKinematic = true;
     }
@@ -20,11 +25,20 @@ public class Bird : MonoBehaviour
 
     private void OnMouseUp()
     {
+        Vector2 currentPosition = GetComponent<Rigidbody2D>().position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+        
+        // Add the bird back to physics control so it can be launched
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<Rigidbody2D>().AddForce(direction * 500);
+        
         GetComponent<SpriteRenderer>().color = Color.white;
     }
 
     private void OnMouseDrag()
     {
+        // Sync the bird position to the mouse position
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
     }
