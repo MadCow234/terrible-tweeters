bd4188d862b6c85d956e383c56d54eb729c59239
MadCow234 <MadCow234@gmail.com>
2021-01-23 03:25:19 -0600

    Remove physics control when Bird starts

--- Terrible Tweeters/Assets/Scripts/Bird.cs
+++ Terrible Tweeters/Assets/Scripts/Bird.cs
@@ -7,7 +7,9 @@ public class Bird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Takes the bird out of physics control
+        // This is so we can aim it and it does not automatically fall to the ground
+        GetComponent<Rigidbody2D>().isKinematic = true;
     }
 
     // Update is called once per frame
