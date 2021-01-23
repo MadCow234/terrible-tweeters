fc8a04fa6d66cbdcf26a2de1cd2d5d1e1fd71d2e
MadCow234 <MadCow234@gmail.com>
2021-01-23 03:29:48 -0600

    Change color of Red Bird on click

--- Terrible Tweeters/Assets/Scripts/Bird.cs
+++ Terrible Tweeters/Assets/Scripts/Bird.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,25 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         // Takes the bird out of physics control
         // This is so we can aim it and it does not automatically fall to the ground
         GetComponent<Rigidbody2D>().isKinematic = true;
     }
 
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
